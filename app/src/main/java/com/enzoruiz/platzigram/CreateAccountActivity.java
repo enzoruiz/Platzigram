1a41c8c156626d83f6e9a2205239342a4ed9dd02
enzo ruiz <enzo.rp.90@gmail.com>
2017-06-20 18:05:04 -0500

    first commit

---
+++ app/src/main/java/com/enzoruiz/platzigram/CreateAccountActivity.java
@@ -0,0 +1,25 @@
+package com.enzoruiz.platzigram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        showToolbar(getResources().getString(R.string.text_toolbar_createaccount), true);
+
+    }
+
+    public void showToolbar(String titulo, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+
+}
