3d7ccc67619b4ad4ee3f9b5657f6dc051bfb6bc1
enzo ruiz <enzo.rp.90@gmail.com>
2017-06-22 17:09:58 -0500

    Creacion de usuario con email y password, y almacenado en Firebase.

---
+++ app/src/main/java/com/enzoruiz/platzigram/PlatzigramApplication.java
@@ -0,0 +1,15 @@
+package com.enzoruiz.platzigram;
+
+import android.app.Application;
+
+/**
+ * Created by Enzo on 22/06/2017.
+ */
+
+public class PlatzigramApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/enzoruiz/platzigram/view/CreateAccountActivity.java
+++ app/src/main/java/com/enzoruiz/platzigram/view/CreateAccountActivity.java
@@ -1,20 +1,93 @@
 package com.enzoruiz.platzigram.view;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.enzoruiz.platzigram.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    private static final String TAG = "CreateAccountActivity";
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private Button btnJoinUs;
+    private TextInputEditText edtEmail;
+    private TextInputEditText edtPassword;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(authStateListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
-
         showToolbar(getResources().getString(R.string.text_toolbar_createaccount), true);
 
+        btnJoinUs = (Button) findViewById(R.id.btnUnete);
+        edtEmail = (TextInputEditText) findViewById(R.id.email);
+        edtPassword = (TextInputEditText) findViewById(R.id.create_password);
+        btnJoinUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccount();
+            }
+        });
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null){
+                    Log.w(TAG, "Usuario Logueado " + firebaseUser.getEmail());
+                }
+                else{
+                    Log.w(TAG, "Usuario NO Logueado");
+                }
+            }
+        };
+
+    }
+
+    private void createAccount() {
+        String email = edtEmail.getText().toString();
+        String password = edtPassword.getText().toString();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(CreateAccountActivity.this, "Usuario creado correctamente", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(CreateAccountActivity.this, "No se pudo crear el usuario", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     public void showToolbar(String titulo, boolean upButton){
